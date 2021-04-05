2f63596cc893f99e74278dcf2e50c58ae1002a78
nyajallen <allen.nya24@gmail.com>
2021-04-05 19:23:48 -0400

    Update database

--- Group-HW5/main.py
+++ Group-HW5/main.py
@@ -1,22 +1,32 @@
 import os
-from flask import Flask, request, render_template
+
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
+
 import util
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-'sqlite:///' +  os.path.join(basedir, 'WebAppsDatbase.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'WebAppsDatbase.db')
 
 db = SQLAlchemy(app)
 
 Base = automap_base()
-Base.prepare(db.engine, reflect = True)
+Base.prepare(db.engine, reflect=True)
 covidData = Base.classes.covid_data
 
-column_names = ["index","What country do you live in?","How old are you?","What is your gender?","To what extent do you feel FEAR due to the coronavirus?","To what extent do you feel ANXIOUS due to the coronavirus?","To what extent do you feel ANGRY due to the coronavirus?","To what extent do you feel HAPPY due to the coronavirus?","To what extent do you feel SAD due to the coronavirus?","Which emotion is having the biggest impact on you?","What makes you feel that way?","What brings you the most meaning during the coronavirus outbreak?","What is your occupation?"]
+column_names = ["index", "What country do you live in?", "How old are you?", "What is your gender?",
+                "To what extent do you feel FEAR due to the coronavirus?",
+                "To what extent do you feel ANXIOUS due to the coronavirus?",
+                "To what extent do you feel ANGRY due to the coronavirus?",
+                "To what extent do you feel HAPPY due to the coronavirus?",
+                "To what extent do you feel SAD due to the coronavirus?",
+                "Which emotion is having the biggest impact on you?", "What makes you feel that way?",
+                "What brings you the most meaning during the coronavirus outbreak?", "What is your occupation?"]
+
 
 @app.route('/')
 def index():
@@ -24,8 +34,9 @@ def index():
     labels = util.cluster_user_data(user_data)
     return render_template('index.html', labels_html=labels, column_html=column_names, data_html=user_data)
 
+
 if __name__ == '__main__':
-	# set debug mode
+    # set debug mode
     app.debug = True
     # your local machine ip
     ip = '127.0.0.1'
--- Group-HW5/util.py
+++ Group-HW5/util.py
@@ -2,12 +2,12 @@ from sklearn.cluster import KMeans
 import numpy as np
 import sqlite3
 
-def query(db, query_group):
 
+def query(db, query_group):
     group = {
-        1: " SELECT index WHERE age <=35 AND gender = 'male'"
-        2: " SELECT index WHERE age >=36 AND gender = 'male'"
-        3: " SELECT index WHERE age <=35 AND gender = 'female'"
+        1: " SELECT index WHERE age <=35 AND gender = 'male'",
+        2: " SELECT index WHERE age >=36 AND gender = 'male'",
+        3: " SELECT index WHERE age <=35 AND gender = 'female'",
         4: " SELECT index WHERE age >=36 AND gender = 'female'"
     }
 
@@ -22,10 +22,10 @@ def query(db, query_group):
 
     return query_results
 
-def get_country(db, country):
 
+def get_country(db, country):
     query = "SELECT index, Country FROM index GROUP BY Country"
-    connection = sqlite3.connect(database_file)
+    connection = sqlite3.connect(db)
     cursor = connection.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
@@ -35,27 +35,28 @@ def get_country(db, country):
 
 
 def cluster_user_data(input_data, emotional_col_start=4, emotional_col_end=9, n_clusters=3):
-	'''
-	This function cluster user data based on KMeans algorithm
-	By default, it will split your data into three groups
-	'''
-	# collect answers for five emotional questions
-	# which are located from 4th col to 9th col
-	emotional_data = [i[emotional_col_start:emotional_col_end] for i in input_data]
-	# use kmeans to cluster data
-	kmeans = KMeans(n_clusters).fit(emotional_data)
-	# return cluster labels
-	return kmeans.labels_
+    """
+    This function cluster user data based on KMeans algorithm
+    By default, it will split your data into three groups
+    """
+    # collect answers for five emotional questions
+    # which are located from 4th col to 9th col
+    emotional_data = [i[emotional_col_start:emotional_col_end] for i in input_data]
+    # use kmeans to cluster data
+    kmeans = KMeans(n_clusters).fit(emotional_data)
+    # return cluster labels
+    return kmeans.labels_
+
 
 def split_user_data(input_data, labels, n_clusters=3):
-	'''
-	this function will split input data into groups
-	based on labels
-	'''
-	result_list = []
-	for i in range(n_clusters):
-		# find indices of each group elements, without [0] the result is tuple
-		tmp_indices = np.where(labels == i)[0]
-		result_list.append([input_data[i] for i in tmp_indices])
+    """
+    this function will split input data into groups
+    based on labels
+    """
+    result_list = []
+    for i in range(n_clusters):
+        # find indices of each group elements, without [0] the result is tuple
+        tmp_indices = np.where(labels == i)[0]
+        result_list.append([input_data[i] for i in tmp_indices])
 
-	return result_list
+    return result_list
